9c5520a96237f39bdb9f270271f463e88779e0ca
Cameron Leech-Thomson <cameronleechthomson@gmail.com>
2022-09-06 01:06:55 +0100

    QoL change

--- assets/js/pausevideo.js
+++ assets/js/pausevideo.js
@@ -8,12 +8,16 @@ function setupVids(){
     console.log("setupVids()");
     vids = document.getElementsByTagName("video");
     console.log("got videos: " + vids);
-    setInterval(checkPageFocus, 5000);
 
-    document.onfocus = function(){
-        console.log("Focus regained, playing videos.");
-        for (var vid of vids){
-            vid.play();
+    // Only bother checking for focus if there are any videos on the page:
+    if (vids.length < 0){
+        setInterval(checkPageFocus, 5000);
+
+        document.onfocus = function(){
+            console.log("Focus regained, playing videos.");
+            for (var vid of vids){
+                vid.play();
+            }
         }
     }
 }
